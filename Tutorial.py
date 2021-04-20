e0e979a56edf880ce512a306f79549483e884180
Shuai Shao <shuai1997@hotmail.se>
2021-04-20 16:07:36 +0200

    Branches

--- Tutorial.py
+++ Tutorial.py
@@ -1,3 +1,5 @@
 # This is a tutorial for how to use git.
 
 # It is not neccessary to backup all of your current changes. Choose which files to backup by staging them. Only these files will be commited.
+
+# In git, multiple people can work on multiple features by using branches.
