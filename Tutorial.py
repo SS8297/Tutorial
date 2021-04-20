88b45045247d75fc23cceb8e932d50f95542b4ba
Shuai Shao <shuai1997@hotmail.se>
2021-04-20 16:03:04 +0200

    Only staged files are commited

--- Tutorial.py
+++ Tutorial.py
@@ -1,1 +1,3 @@
 # This is a tutorial for how to use git.
+
+# It is not neccessary to backup all of your current changes. Choose which files to backup by staging them. Only these files will be commited.
