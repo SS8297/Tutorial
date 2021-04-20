f1319f1642d3f4b6117c1b767fff2af10f5d9a38
Shuai Shao <shuai1997@hotmail.se>
2021-04-20 16:12:40 +0200

    Merge

--- Tutorial.py
+++ Tutorial.py
@@ -3,3 +3,5 @@
 # It is not neccessary to backup all of your current changes. Choose which files to backup by staging them. Only these files will be commited.
 
 # Changes on branches do not affect the main branch. This way, it doesn't matter if you break the code, you can always go back to main to run it.
+
+# After you're done working on a branch, you can merge it back into main.
