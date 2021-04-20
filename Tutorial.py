3a1010b73361d16c7523d413f43a89c679b2f17b
Shuai Shao <shuai1997@hotmail.se>
2021-04-20 16:08:57 +0200

    do not affect main

--- Tutorial.py
+++ Tutorial.py
@@ -1,3 +1,5 @@
 # This is a tutorial for how to use git.
 
 # It is not neccessary to backup all of your current changes. Choose which files to backup by staging them. Only these files will be commited.
+
+# Changes on branches do not affect the main branch. This way, it doesn't matter if you break the code, you can always go back to main to run it.
