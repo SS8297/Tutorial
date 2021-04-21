ee3fe69ff9fe94ae4cecfb450ca50a4dc974220b
Ellen <siggstedtellen@gmail.com>
2021-04-21 11:46:36 +0200

    Lagt till rad

--- Tutorial.py
+++ Tutorial.py
@@ -7,3 +7,5 @@
 # Changes on branches do not affect the main branch. This way, it doesn't matter if you break the code, you can always go back to main to run it.
 
 # In git, multiple people can work on multiple features by using branches.
+
+#Testar att skriva in den här raden
